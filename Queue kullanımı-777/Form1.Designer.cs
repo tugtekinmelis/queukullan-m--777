627458155efba5ac1dbd2e2d401e87205951605e
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2024-02-06 10:30:05 +0300

    Add files via upload

---
+++ Queue kullanımı-777/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Queue_kullanımı_777
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(34, 57);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(127, 24);
+            this.txtAd.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(34, 106);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(127, 51);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Sıra Al";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGetir.Location = new System.Drawing.Point(258, 106);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(127, 51);
+            this.btnGetir.TabIndex = 2;
+            this.btnGetir.Text = "Müşteri Çağır";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAd.Location = new System.Drawing.Point(255, 57);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(52, 18);
+            this.lblAd.TabIndex = 3;
+            this.lblAd.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.Label lblAd;
+    }
+}
+
---
+++ Queue kullanımı-777/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Queue_kullanımı_777
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> musteri = new Queue<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            musteri.Enqueue(txtAd.Text);
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            if (musteri.Count > 0)
+            {
+                lblAd.Text = musteri.Dequeue();
+            }
+            else
+            {
+                MessageBox.Show("işin çok hızlı bitti aferim :)");
+            }
+        }
+    }
+}
